ba062c8a5fbf5d9f3aa1fd035e50fea376795b7a
kdowd <kevin.dowd@gmail.com>
2024-05-26 21:34:26 +1200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonBindWithConverter
+{
+
+    public partial class MainWindow : Window
+    {
+
+
+
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+
+            // Person? temp = PersonInstance.ObjectInstance as Person;
+            // temp.Person_Age = 666;
+
+
+            // Person p =
+            // 
+            MyPerson.Person_Age = 666;
+
+
+
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem? temp = sender as MenuItem;
+            MessageBox.Show(temp?.Header.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button ?? new Button();
+
+            MessageBox.Show(btn.Content.ToString());
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PersonBindWithConverter
+{
+    class Person
+    {
+        public int Person_Age { get; set; } = 34;
+        public string Person_Firstname { get; set; } = "jane";
+        public string Person_Lastname { get; set; } = "smith";
+        public Person()
+        {
+
+
+        }
+    }
+}
+
